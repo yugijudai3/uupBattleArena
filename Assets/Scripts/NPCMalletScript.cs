4e26c009ac88f8fed2a2c77d19ee740aadec8368
hideya satake <yugiohocg123@gmail.com>
2019-11-21 14:11:11 +0900

    マレットのスクリプト

--- Assets/Scripts/NPCMalletScript.cs
+++ Assets/Scripts/NPCMalletScript.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NPCMalletScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * speed);
     }
 }
