425039e8caa3743b4b6eb208622571e683e28fe1
hideya satake <yugiohocg123@gmail.com>
2019-11-28 14:20:03 +0900

    パックの生成・初期移動

--- Assets/Scripts/PackScript.cs
+++ Assets/Scripts/PackScript.cs
@@ -12,6 +12,8 @@ public class PackScript : MonoBehaviour
     {
         Clone = Instantiate(Pack, new Vector3(0, 0.15f ,0), Quaternion.identity);
         Clone.transform.Rotate(new Vector3(0, Random.Range(360, -360), 0));
+        Rigidbody rb = Clone.GetComponent<Rigidbody>();
+        rb.AddForce(Clone.transform.forward * 10, ForceMode.Impulse);
     }
 
     void Update()
