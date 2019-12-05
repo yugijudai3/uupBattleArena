52a57fc038b047a338c347d440691c8d081f67b9
uupPCclass <otounkohahiroki@gmail.com>
2019-12-05 14:15:44 +0900

    GameManager関連の変更

--- Assets/Scripts/PackScript.cs
+++ Assets/Scripts/PackScript.cs
@@ -7,17 +7,33 @@ public class PackScript : MonoBehaviour
     public GameObject Pack;
     private GameObject Clone;
     public float speed;
+    public float time;
+    public bool waitFlag;
 
     void Start()
     {
+        speed = 5;
         Clone = Instantiate(Pack, new Vector3(0, 0.15f ,0), Quaternion.identity);
         Clone.transform.Rotate(new Vector3(0, Random.Range(360, -360), 0));
         Rigidbody rb = Clone.GetComponent<Rigidbody>();
-        rb.AddForce(Clone.transform.forward * 10, ForceMode.Impulse);
+        rb.AddForce(Clone.transform.forward * speed, ForceMode.Impulse);
     }
 
     void Update()
     {
+        Debug.Log(Time.time);
+
+        time = Mathf.Floor(Time.time);
+        if (time % 4 == 0 && waitFlag == false && time != 0)
+        {
+            Clone = Instantiate(Pack, new Vector3(0, 0.15f, 0), Quaternion.identity);
+            Clone.transform.Rotate(new Vector3(0, Random.Range(360, -360), 0));
+            Rigidbody rb = Clone.GetComponent<Rigidbody>();
+            rb.AddForce(Clone.transform.forward * speed, ForceMode.Impulse);
+            waitFlag = true;
+        } else if (time % 4 != 0) {
+            waitFlag = false;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
