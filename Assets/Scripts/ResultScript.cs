ab31c16dfe92c90ae7bd25ee8f2b5aa8ab5e3f9f
hideya satake <yugiohocg123@gmail.com>
2020-01-17 15:36:48 +0900

    スコア最終表示

--- Assets/Scripts/ResultScript.cs
+++ Assets/Scripts/ResultScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResultScript : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class ResultScript : MonoBehaviour
     public int score2;
     public int score3;
     public int score4;
+    public Text playerText;
+    public Text CPU1Text;
+    public Text CPU2Text;
+    public Text CPU3Text;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,11 @@ public class ResultScript : MonoBehaviour
         score2 = manager.score[1];
         score3 = manager.score[2];
         score4 = manager.score[3];
+
+        playerText.text = score1.ToString();
+        CPU1Text.text = score2.ToString();
+        CPU2Text.text = score3.ToString();
+        CPU3Text.text = score4.ToString();
     }
 
     // Update is called once per frame
